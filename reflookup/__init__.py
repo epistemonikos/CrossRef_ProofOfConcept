e942b93fe163bb890d0106f580d7ac9ad087bb1c
Manuel Olguin <molguin@dcc.uchile.cl>
2016-10-05 14:38:51 -0300

    get secret_key from env

--- reflookup/__init__.py
+++ reflookup/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_restful import Api
+import os
 
 app = Flask(__name__)
 app.config['CROSSREF_URI'] = 'http://api.crossref.org/works'
-app.secret_key = 'blah blah'  # TODO: Replace with real, pseudorandom, string
+app.secret_key = os.environ.get('REFSERVICE_SECRETKEY', '12345')
 app.config['API_PREFIX'] = '/api/v1'
 api = Api(app)
 
