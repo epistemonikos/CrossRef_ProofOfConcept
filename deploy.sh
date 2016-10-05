fbc0002aec92d6aa9a619b38b682e7b6c87fae55
Manuel Olguin <molguin@dcc.uchile.cl>
2016-10-05 13:28:05 -0300

    fix deploy script a bit

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-echo "Deploying Reference Lookup Service\n"
+echo "Deploying Reference Lookup Service"
 git pull
 git checkout master
 if [ ! -d "./venv" ]; then
     virtualenv --python=python3.5 ./venv
 fi
 ./venv/bin/pip install -r requirements.txt
-echo "Restarting services...\n"
+echo "Restarting services..."
 sudo service referencelookup restart
 sudo service nginx restart
-echo "All done!\n"
+echo "All done!"
