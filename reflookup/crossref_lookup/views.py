83326d63983332f32dc6ba3c4ca70456c4341f79
Manuel Olguin <molguin@dcc.uchile.cl>
2016-10-06 15:04:46 -0300

    a tiny bit of documentation

--- reflookup/crossref_lookup/views.py
+++ reflookup/crossref_lookup/views.py
@@ -11,8 +11,20 @@ from rating.rating import Rating
 from reflookup.search_form import CrossRefForm
 from reflookup.ris_utils.convert import dict2ris, RISTypeException
 
+"""
+This file contains the endpoint resources for looking up references in
+CrossRef.
+"""
+
 
 def cr_citation_lookup(citation):
+    """
+    This function does the actual CrossRef API call to search for a given
+    citation, and returns the first (and thus, according to CR, the best)
+    result.
+    :param citation: Citation to look up in CR.
+    :return: A Python dict representing the best result offered by CrossRef.
+    """
     params = {'query': citation}
     url = app.config['CROSSREF_URI']
 
@@ -23,7 +35,7 @@ def cr_citation_lookup(citation):
     rv = req.json()
 
     if len(rv['message']['items']) < 1:
-        abort(404,'No results found for query.')
+        abort(404, 'No results found for query.')
 
     result = rv['message']['items'][0]
     result['rating'] = Rating(citation, result).value()
@@ -33,6 +45,15 @@ def cr_citation_lookup(citation):
 
 @api.representation('application/x-research-info-systems')
 def serve_ris(data, code, headers=None):
+    """
+    Helper function to parse a CrossRef return JSON into a valid RIS.
+    Deprecated. TODO: Replace with general-purpose parsing function.
+    :param data: Data to pack in a flask response.
+    :param code: HTTP status code of the response.
+    :param headers: Headers of the response.
+    :return: A packed response containing the parsed RIS document, or,
+    if it fails, the original JSON data.
+    """
     try:
         ris_data = dict2ris(data)
         resp = make_response(ris_data, code)
@@ -44,6 +65,9 @@ def serve_ris(data, code, headers=None):
 
 
 class CrossRefLookupResource(Resource):
+    """
+    This resource represents the /crsearch endpoint on the API.
+    """
     def __init__(self):
         self.post_parser = reqparse.RequestParser()
         self.post_parser.add_argument('ref', type=str, required=True,
@@ -60,6 +84,9 @@ class CrossRefLookupResource(Resource):
 
 
 class CrossRefSearchForm(Resource):
+    """
+    This resource represents the / endpoint, and it's associated form.
+    """
     def __init__(self):
         self.parser = reqparse.RequestParser()
         self.parser.add_argument('query', location='form')
