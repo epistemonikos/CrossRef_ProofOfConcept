6adb084ce491c45a6f891efb9eb6bee7ff4e62fe
Ricardo Matías Peralta Muñoz <mperalta92@gmail.com>
2016-10-13 11:32:04 -0300

    fix  test_mendeley

--- tests/datatests/tsv_mendeley_test.py
+++ tests/datatests/tsv_mendeley_test.py
@@ -4,24 +4,33 @@ import requests
 import pprint
 import os
 
-rrsr = open('./src/random_results_systematic_review')
+with open('output_correctos.tsv', 'w') as output_correctos:
+    with open('output_incorrectos.tsv', 'w') as output_incorrectos:
+        with open('./src/random_results_systematic_review') as rrsr:
+            count = 0
+            for line in rrsr:
+                count += 1
+                if count%20 == 0:
+                    print("Estoy vivo. Llevo: {0}".format(count))
+                sline = line.split('\t')
+                try:
+                    if len(sline) > 1:
+                        episte_doi = sline[0] or '-'
+                        reference = ''.join(sline[1:]) or '-'
+                        x = requests.get('http://localhost:5001/api/v1/mdsearch', params={
+                            'ref' : reference
+                        })
+                        rating = x.json().get('rating', {}).get('total', 0)
+                        mendeley_doi = x.json().get('ids', {}).get('doi', '-')
+                        if "doi.org" in mendeley_doi:
+                            index = mendeley_doi.index(".org/") + 5
+                            mendeley_doi = mendeley_doi[index:]
 
-line = rrsr.readline()
-while line:
-    sline = line.split('\t')
-    try:
-        if len(sline) > 1:
-            episte_doi = sline[0] or '-'
-            reference = ''.join(sline[1:]) or '-'
-            x = requests.get('http://localhost:5001/api/v1/mdsearch', params={
-                'ref' : reference
-            })
-            rating = x.json().get('rating', {}).get('total', 0)
-            mendeley_doi = x.json().get('ids', {}).get('doi', '-')
-            if "doi.org" in mendeley_doi:
-                index = mendeley_doi.index(".org/") + 5
-                mendeley_doi = mendeley_doi[index:]
-            print("%s\t%s\t%s\t%.2f\t%s" % ('Mendeley', mendeley_doi, episte_doi, rating, reference))
-    except:
-        pass
-    line = rrsr.readline()
+                        new_line = u"{}\t{}\t{}\t{:.2f}\t{4:s}".format('Mendeley', mendeley_doi, episte_doi,
+                                                                                 rating, reference)
+                        if mendeley_doi == episte_doi:
+                            output_correctos.write(new_line)
+                        else:
+                            output_incorrectos.write(new_line)
+                except:
+                    pass
