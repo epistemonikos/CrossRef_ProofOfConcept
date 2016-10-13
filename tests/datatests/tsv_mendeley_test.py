1f78e6081d053710d68f127d9e1966c80fad657f
Ricardo Matías Peralta Muñoz <mperalta92@gmail.com>
2016-10-13 10:25:53 -0300

    add try/catch test mendeley and crossref

--- tests/datatests/tsv_mendeley_test.py
+++ tests/datatests/tsv_mendeley_test.py
@@ -9,13 +9,16 @@ rrsr = open('./src/random_results_systematic_review')
 line = rrsr.readline()
 while line:
     sline = line.split('\t')
-    if len(sline) > 1:
-        episte_doi = sline[0] or '-'
-        reference = ''.join(sline[1:]) or '-'
-        x = requests.get('http://0.0.0.0:5001/api/v1/mdsearch', params={
-            'ref' : reference
-        })
-        rating = x.json().get('rating', {}).get('total', 0)
-        mendeley_doi = x.json().get('ids', {}).get('doi', '-')
-        print("%s\t%s\t%s\t%.2f\t%s" % ('Mendeley', mendeley_doi, episte_doi, rating, reference))
+    try:
+        if len(sline) > 1:
+            episte_doi = sline[0] or '-'
+            reference = ''.join(sline[1:]) or '-'
+            x = requests.get('http://localhost:5001/api/v1/mdsearch', params={
+                'ref' : reference
+            })
+            rating = x.json().get('rating', {}).get('total', 0)
+            mendeley_doi = x.json().get('ids', {}).get('doi', '-')
+            print("%s\t%s\t%s\t%.2f\t%s" % ('Mendeley', mendeley_doi, episte_doi, rating, reference))
+    except:
+        pass
     line = rrsr.readline()
