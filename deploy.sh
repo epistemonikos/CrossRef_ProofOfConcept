a44ed63c0f239dd2c585beae53e4e09c5a6d1a69
Manuel Olguin <molguin@dcc.uchile.cl>
2016-10-19 16:40:54 -0300

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 echo "Deploying Reference Lookup Service"
+echo "Killing worker..."
+killall rq
 git pull
 git checkout master
 if [ ! -d "./venv" ]; then
     virtualenv --python=python3.5 ./venv
 fi
 ./venv/bin/pip install -r requirements.txt
+echo "Starting worker..."
+screen -d -m ./venv/bin/rq worker
 echo "Restarting services..."
 sudo service referencelookup restart
 sudo service nginx restart
