cf26dae05022aa85c9ff78e815e5f602bbe3fb8a
Ricardo Matías Peralta Muñoz <mperalta92@gmail.com>
2016-10-25 13:59:59 -0300

    fix tests and add new test for search endpoint

--- tests/unittests/lookuptest.py
+++ tests/unittests/lookuptest.py
@@ -8,17 +8,18 @@ from urllib.parse import quote
 class LookupTest(tests.unittests.basetest.BaseTest):
     #this test check an especial reference where crossref service must win
     def test_lookup(self):
-        citation = quote(self.test_cite, safe='')
-        params = {'ref': citation}
+        #citation = quote(self.test_cite, safe='')
+        params = {'ref': self.test_cite}
         ret = self.app.post(self.prefix + '/search', data=params)
         assert ret
         jdata = json.loads(ret.data)
         assert jdata
-        url = jdata.get('ids', None)
-        if url:
-            url = url.get('doi',None)
-        assert url
-        assert url == self.cr_doi
+        url = jdata.get('result', None)
 
+        assert url
+        url = url[0].get('ids')
+        assert url.get('doi', None) == self.cr_doi
+
+    
 if __name__ == '__main__':
     unittest.main()
