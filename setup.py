091db8597b0fd2b95b721ed3cb46ef577903a4e4
Manuel Olguin <molguin@dcc.uchile.cl>
2016-10-26 13:25:14 -0300

    setup file for setuptools deployment

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='Reference Lookup Service',
+    version='1.0beta1',
+    long_description=__doc__,
+    packages=['reflookup'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'flask-wtf',
+        'requests',
+        'flask-restful',
+        'gunicorn',
+        'stop-words',
+        'unidecode',
+        'bs4',
+        'rq',
+        'redis'
+    ]
+)
