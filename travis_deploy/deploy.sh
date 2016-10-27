0ea82dce4c04fbf3ec9f954cc3018657260c8d37
Manuel Olguin <molguin@dcc.uchile.cl>
2016-10-27 14:23:06 -0300

    travis deploy changed to after_success

--- travis_deploy/deploy.sh
+++ travis_deploy/deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-echo "Starting deploy on server."
-eval "$(ssh-agent -s)" #start the ssh agent
-echo "Adding key to ssh-agent."
-chmod 600 travis_deploy/travis_ci.rsa # this key should have push access
-ssh-add travis_deploy/travis_ci.rsa
-echo "Deploying..."
-invoke deploy
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    echo "Starting deploy on server."
+    eval "$(ssh-agent -s)" #start the ssh agent
+    echo "Adding key to ssh-agent."
+    chmod 600 travis_deploy/travis_ci.rsa # this key should have push access
+    ssh-add travis_deploy/travis_ci.rsa
+    echo "Deploying..."
+    invoke deploy
+fi
