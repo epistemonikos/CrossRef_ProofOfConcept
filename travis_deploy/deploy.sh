3a46091e339ed67d94e1bb9d49bd891eba61178e
Manuel Olguin <molguin@dcc.uchile.cl>
2016-10-27 14:12:59 -0300

    travis deploy

--- travis_deploy/deploy.sh
+++ travis_deploy/deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+echo "Starting deploy on server."
 eval "$(ssh-agent -s)" #start the ssh agent
+echo "Adding key to ssh-agent."
 chmod 600 travis_deploy/travis_ci.rsa # this key should have push access
 ssh-add travis_deploy/travis_ci.rsa
+echo "Deploying..."
 invoke deploy
