854c88a05869e55908eb4c0ed69149a49ef6a2c4
Manuel Olguin <molguin@dcc.uchile.cl>
2016-10-27 17:32:52 -0300

    deploy through tarball and ssh

--- travis_deploy/deploy.sh
+++ travis_deploy/deploy.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = "master" ]; then
-    echo "Starting deploy on server."
-    invoke deploy
+    echo "Starting deploy."
+    echo "Packing distribution package..."
+    cd ..
+    DIST="/tmp/reflookup.tar.gz"
+    tar czfv "$DIST" --exclude-vcs --exclude='.idea' --exclude='venv' --exclude="__pycache__" ReferenceLookupService
+    cd ReferenceLookupService
+    echo "Pushing to remote..."
+    scp -o StrictHostKeyChecking=no "$DIST" "ubuntu@52.3.221.80:$DIST"
+    cat travis_deploy/remote_commands_deploy.sh|ssh -o StrictHostKeyChecking=no ubuntu@52.3.221.80
+    echo "Done"
 fi
---
+++ travis_deploy/remote_commands_deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+rm -rf /tmp/www_reflookup
+mkdir /tmp/www_reflookup
+virtualenv --python=python3.5 /tmp/www_reflookup/venv
+cd /tmp
+tar xzmfv /tmp/reflookup.tar.gz
+cp -r /tmp/ReferenceLookupService/* /tmp/www_reflookup/
+rm -rf /tmp/ReferenceLookupService /tmp/reflookup.tar.gz
+cd /tmp/www_reflookup
+./venv/bin/pip install -r requirements.txt
+killall gunicorn #TODO: FIX
+screen -d -m ./venv/bin/gunicorn --workers 3 --bind 0.0.0.0:5000 reflookup.wsgi:app --error-logfile errors.log
+killall rq #TODO: FIX TOO
+screen -d -m ./venv/bin/rq worker
