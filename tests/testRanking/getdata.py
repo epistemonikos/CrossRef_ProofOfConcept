3e5c5a39a2cc491590b77bf43e9d305c74e05783
Tomas Saez B <tomas.saezbin@gmail.com>
2016-11-03 17:49:49 -0300

    added getdata.py resolved line counting

--- tests/testRanking/getdata.py
+++ tests/testRanking/getdata.py
@@ -5,55 +5,68 @@ import pprint
 import os
 import csv
 
+counter_file = open('count', 'r')
+count = counter_file.read() or 0
+counter_file.close()
+
 with open('data.tsv', 'w') as output_citas:
   with open('input.tsv') as input_citas:
-    count = 0
-    for line in input_citas:
-      count += 1
-      line.replace ("\n", " ")
+    
+      ##input_citas.skip_lines(count)
 
-      if count%20 == 0:
-        print("Estoy vivo. Llevo: {0}".format(count))
-      sline = line.split('\t')
-     
 
-      if len(sline) > 1:
-        cita = sline[0] or '-'
-        episte_doi = sline[1] or '-'
-        episte_pubmedId = sline[2] or '-'
-        episte_clasification = sline[3] or '-'
-        episte_id = sline[4] or '-'
-
-        if sline[5] == "\n":
-          episte_ref_id = '-'
-        else:
-          episte_ref_id = sline[5] or '-'
+      for _ in range(count):
+        next(input_citas)
         
-        try:
-          x = requests.get('http://52.3.221.80/api/v1/crsearch', params={
-            'ref' : cita
-            },
-            headers= {
-            'Accept-Encoding': 'base64'
-            })
-          crossref_json = x.text
-          cr_line = u"{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(cita, episte_doi, episte_pubmedId, episte_clasification, episte_id, episte_ref_id.replace("\n","_"), crossref_json)
+      for line in input_citas:
+        
+        line.replace ("\n", " ")
+        sline = line.split('\t')
+       
+        if len(sline) > 1:
+          cita = sline[0] or '-'
+          episte_doi = sline[1] or '-'
+          episte_pubmedId = sline[2] or '-'
+          episte_clasification = sline[3] or '-'
+          episte_id = sline[4] or '-'
 
-        except HTTPException:
-          cr_line = "LINEERROR"
+          if sline[5] == "\n":
+            episte_ref_id = '-'
+          else:
+            episte_ref_id = sline[5] or '-'
+          
+          try:
+            x = requests.get('http://52.3.221.80/api/v1/crsearch', params={
+              'ref' : cita
+              },
+              headers= {
+              'Accept-Encoding': 'base64'
+              })
+            crossref_json = x.text
+            cr_line = u"{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(cita, episte_doi, episte_pubmedId, episte_clasification, episte_id, episte_ref_id.replace("\n","_"), crossref_json)
 
-        try:
-          y = requests.get('http://52.3.221.80/api/v1/mdsearch', params={
-            'ref' : cita
-            },
-            headers= {
-            'Accept-Encoding': 'base64'
-            })
-          mendeley_json = y.text
-          md_line = u"{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(cita, episte_doi, episte_pubmedId, episte_clasification, episte_id, episte_ref_id.replace("\n","_"), mendeley_json)
+          except:
+            cr_line = "LINEERROR"
 
-        except HTTPException:
-          md_line = "LINEERROR"
+          try:
+            y = requests.get('http://52.3.221.80/api/v1/mdsearch', params={
+              'ref' : cita
+              },
+              headers= {
+              'Accept-Encoding': 'base64'
+              })
+            mendeley_json = y.text
+            md_line = u"{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(cita, episte_doi, episte_pubmedId, episte_clasification, episte_id, episte_ref_id.replace("\n","_"), mendeley_json)
 
-        output_citas.write(cr_line)
-        output_citas.write(md_line)  
+          except:
+            md_line = "LINEERROR"
+
+          output_citas.write(cr_line)
+          output_citas.write(md_line)
+
+        count += 1
+        counter_file = open('count', 'w')
+        counter_file.write(count)
+        counter_file.close()
+        if count%20 == 0:
+          print("Estoy vivo. Llevo: {0}".format(count))
