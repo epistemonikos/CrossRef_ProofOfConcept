f5051c98fa887af86b492264cf18022c55a81cb9
Felipe Rubilar <felipe.rubilar@ug.uchile.cl>
2016-11-03 14:30:26 -0300

    Removing old files

--- quick_references/resources/views.py
+++
@@ -1,5 +0,0 @@
-from quick_references import api, app
-from reflookup.resources.scopus import ScopusResource
-
-prefix = app.config['API_PREFIX']
-api.add_resource(ScopusResource, prefix + '/refs/scopus')
