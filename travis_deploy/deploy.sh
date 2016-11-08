6cd7ad8d9bc5542bec3207ffa419117471a17b2c
Manuel Olguin <molguin@dcc.uchile.cl>
2016-11-08 10:40:19 -0300

    debugging

--- travis_deploy/deploy.sh
+++ travis_deploy/deploy.sh
@@ -7,7 +7,7 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
     tar czfv "$DIST" --exclude-vcs --exclude='.idea' --exclude='venv' --exclude="__pycache__" ReferenceLookupService
     cd ReferenceLookupService
     echo "Pushing to remote..."
-    scp -o StrictHostKeyChecking=no "$DIST" "ubuntu@52.3.221.80:$DIST"
+    scp -o StrictHostKeyChecking=no "$DIST" "ubuntu@52.3.221.80:$DIST" -vvv
     cat travis_deploy/remote_commands_deploy.sh|ssh -o StrictHostKeyChecking=no ubuntu@52.3.221.80
     echo "Done"
 fi
