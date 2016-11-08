bf2e75823415f922baa94c69c1045d20144631fb
Manuel Olguin <molguin@dcc.uchile.cl>
2016-11-08 11:52:22 -0300

    text pid storing

--- travis_deploy/remote_commands_deploy.sh
+++ travis_deploy/remote_commands_deploy.sh
@@ -6,11 +6,11 @@ tar xzmfv ./reflookup.tar.gz
 mv /tmp/ReferenceLookupService /tmp/www_reflookup
 virtualenv --python=python3.5 /tmp/www_reflookup/venv
 cd /tmp/www_reflookup
-echo ./.env
+cat ./.env
 source ./.env
 ./venv/bin/pip install -r requirements.txt
 killall gunicorn #TODO: FIX
 echo "Starting service"
-screen -d -m ./venv/bin/gunicorn --workers 3 --bind 0.0.0.0:5000 reflookup.wsgi:app --error-logfile errors.log
+screen -d -m "./venv/bin/gunicorn --workers 3 --bind 0.0.0.0:5000 reflookup.wsgi:app --error-logfile errors.log & echo $! > ./gunicorn.pid"
 killall rq #TODO: FIX TOO
-screen -d -m ./venv/bin/rq worker
+screen -d -m "./venv/bin/rq worker & echo $! > worker.pid"
