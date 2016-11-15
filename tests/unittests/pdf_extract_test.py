f11e572eb8a472ad1261d4c51565064ccb16b2a1
sam <sam.hv@icloud.com>
2016-11-15 12:55:11 -0300

    add: pdf_extract_test para ser ejecutado con invoke

--- tests/unittests/pdf_extract_test.py
+++ tests/unittests/pdf_extract_test.py
@@ -6,47 +6,39 @@ import os
 import requests
 import time
 from tests.unittests.sources.output_pdf_extract_test import result
+from tests.unittests.basetest import BaseTest
 
-class PdfExtractTest(unittest.TestCase):
+class PdfExtractTest(BaseTest):
 
     def setUp(self):
-        app.config['TESTING'] = True
-        self.app = app.test_client()
-        self.prefix = app.config['API_PREFIX']
+        super(PdfExtractTest, self).setUp()
         self.pdf_path = os.path.join(os.path.dirname(__file__), 'sources/pdf_extract_test.pdf')
         self.references_path = os.path.join(os.path.dirname(__file__), 'sources/output_pdf_extract_test')
         self.pdf = open(self.pdf_path, 'rb')
         self.references = result
         self.url_base = 'http://0.0.0.0:5001'
 
-    def test_base(self):
-        if not self.app:
-            raise AssertionError
-
     def test_pdf_extract(self):
         files = {
             'pdf_file' : self.pdf
         }
         ret = requests.post(self.url_base + self.prefix + '/refs/pdf', files=files)
-        if not ret:
-            raise AssertionError
+        assert ret
         jdata = ret.json()
-        if not jdata:
-            raise AssertionError
+        assert jdata
         token = jdata.get('job', None)
-        if not token:
-            raise AssertionError
+        assert token
         data = {
             'id' : token
         }
-        while True:    
+        while True:
             ret = requests.get(self.url_base + '/api/v1/job', data=data)
             jdata = ret.json()
             if jdata.get('done'):
                 references = jdata.get('result') or []
                 self.assertEqual(references, self.references)
                 break
-            time.sleep(10)
+            time.sleep(3)
 
 if __name__ == '__main__':
     unittest.main()
