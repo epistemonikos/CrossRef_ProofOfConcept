c6953d16f2c74f525bbf8b376d83b6c87c0337d1
Ricardo Matías Peralta Muñoz <mperalta92@gmail.com>
2016-11-22 13:49:31 -0300

    documentation crossreftest

--- tests/unittests/crossreftest.py
+++ tests/unittests/crossreftest.py
@@ -7,8 +7,13 @@ from tests.unittests import basetest
 
 
 class CrossRefTest(basetest.BaseTest):
-    #is the crossref endpoint works?
+    """
+    This is a testing class for crsearch endpoint, it works with crossref API
+    """
     def test_crossreflookup(self):
+        """
+        This test make sure the correctness and activation of crsearch endpoint
+        """
         citation = quote(self.test_cite, safe='')
         params = {'ref': citation}
 
@@ -21,6 +26,9 @@ class CrossRefTest(basetest.BaseTest):
         self.assertEqual(url[0].get('ids', None).get('doi', None), self.cr_doi)
 
     def test_crossrefcrash(self):
+        """
+        This test probe an incorrect input on crsearch endpoint, and expect an 404 HTTP response code
+        """
         params = {'ref': 'kangfkadslnfklasdmnfglkamfklafñlmnadlkfmsaklf'}
         ret = self.app.post(self.prefix + '/crsearch', data=params)
         assert ret
