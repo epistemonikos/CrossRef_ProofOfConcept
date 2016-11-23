6cea942ad4b072dd56835991b4aa79b1a7fea7a9
Felipe Rubilar <felipe.rubilar@ug.uchile.cl>
2016-11-23 15:02:07 -0300

    WileyParserTest + expected output

--- tests/unittests/wiley_test.py
+++ tests/unittests/wiley_test.py
@@ -9,9 +9,8 @@ class WileyParserTest(parsertest.BaseParserTest):
 
     def setUp(self):
         reflookup.app.config['TESTING'] = True
-        # TODO: bucar una URL para testear wiley + output esperado
-        self.url = "http://www.google.cl/"
-        self.path = os.path.join(os.path.dirname(__file__), 'sources/bmc.json')
+        self.url = "http://onlinelibrary.wiley.com/doi/10.1111/acel.12547/full"
+        self.path = os.path.join(os.path.dirname(__file__), 'sources/wiley.json')
         with open(self.path, 'r') as result:
             self.data = json.load(result)
         self.parser = WileyParser()
