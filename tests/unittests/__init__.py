79c7d0b430e422e683ad0b0944e7487bc6664c9a
Felipe Rubilar <felipe.rubilar@ug.uchile.cl>
2016-11-23 15:02:07 -0300

    WileyParserTest + expected output

--- tests/unittests/__init__.py
+++ tests/unittests/__init__.py
@@ -8,6 +8,7 @@ from tests.unittests.pdf_extract_test import PdfExtractTest
 from tests.unittests.parsertest import BaseParserTest
 from tests.unittests.bmc_test import BMCParserTest
 from tests.unittests.springer_test import SpringerParserTest
+from tests.unittests.wiley_test import WileyParserTest
 
 if __name__ == '__main__':
     unittest.main()
