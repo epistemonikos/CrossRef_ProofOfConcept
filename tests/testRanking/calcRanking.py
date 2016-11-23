9f983e35ab99685848c5a62a109b09112a3e0127
Fabian Mosso Chavez <fmosso@klabs.cl>
2016-11-23 15:44:22 -0300

    refactored

--- tests/testRanking/calcRanking.py
+++ tests/testRanking/calcRanking.py
@@ -10,12 +10,16 @@ import binascii
 AVGLENGTH = 220
 
 weight1= {'title' :0.65, 'authors' : 0.2 , 'year': 0.15}
-weight2= {'title' :0.1, 'authors' : 0.9 , 'year': 0.0}
+weight2= {'title' :0.5, 'authors' : 0.0 , 'year': 0.5}
 weight3= {'title' :0.1, 'authors' : 0.0 , 'year': 0.9}
 weight4= {'title' :0.9, 'authors' : 0.05 , 'year': 0.05}
 
 weights = [weight1,weight2,weight3,weight4]
 csv.field_size_limit(500 * 1024 * 1024)
+
+def setWeight(t=0.0,a=0.0,y=0.0):
+    return {'title' :t, 'authors' : a , 'year': y}
+
 def weightLength(len):
     if len >= AVGLENGTH:
         len=AVGLENGTH
@@ -27,25 +31,32 @@ def calcRating(ratings, weight,length):
     lenWeight = weightLength(length)
     return (ratings['title']*weight['title'] + ratings['authors']*weight['authors'] + ratings['year']*weight['year'])*lenWeight
 
-i=0
 
-with open('data140k.tsv') as tsvin, open('rankingResults140k.tsv', 'wt') as csvout:
-    tsvin = csv.reader(tsvin, delimiter='\t')
-    csvout = csv.writer(csvout)
-    csvout.writerow(["Original","w1(t:0.65 a:0.2 y:0.15)", "w2(t:0.1 a:0.9 y:0.0)", "w3(t:0.1 a:0.0 y:0.9)","w4(t:0.9 a:0.05 y:0.05)"])
-    for row in tsvin:
-        i += 1
-        numberOfCollum = len(row)
-        if numberOfCollum == 0:
-            continue
-        citlength = len(row[0])
-        standartJSON = row[numberOfCollum-1]
-        try:
-            ratings = json.loads(base64.urlsafe_b64decode(standartJSON).decode("utf-8"))['result'][0]
-        except (UnicodeDecodeError,binascii.Error,json.decoder.JSONDecodeError):
-            ratings = {}
+def calRankings(listOfRankings):
+    with open('data140k.tsv') as tsvin, open('rankingResults140k.tsv', 'wt') as csvout:
+        tsvin = csv.reader(tsvin, delimiter='\t')
+        csvout = csv.writer(csvout)
+        first_row = ["Original"]
+        def wtoStr(weight):
+            return 't:'+str(weight['title'])+ ' a:'+str(weight['authors'])+' y:'+str(weight['year'])
+        for weight in listOfRankings:
+            first_row.append(wtoStr(weight))
+        csvout.writerow(first_row)
+        print(first_row)
+        for row in tsvin:
+            numberOfCollum = len(row)
+            if numberOfCollum == 0:
+                continue
+            citlength = len(row[0])
+            standartJSON = row[numberOfCollum-1]
+            try:
+                ratings = json.loads(base64.urlsafe_b64decode(standartJSON).decode("utf-8"))['result'][0]
 
-        rankings = [ratings.get('rating',{}).get('total',0)]
-        for weight in weights:
-            rankings.append(calcRating(ratings.get('rating',{'year': 0, 'title': 0, 'total': 0, 'authors': 0}),weight,citlength))
-        csvout.writerow(rankings)
+            except (UnicodeDecodeError,binascii.Error,json.decoder.JSONDecodeError):
+                ratings = {}
+            rankings = [ratings.get('rating',{}).get('total',0)]
+            for weight in listOfRankings:
+                rankings.append(calcRating(ratings.get('rating',{'year': 0, 'title': 0, 'total': 0, 'authors': 0}),weight,citlength))
+            csvout.writerow(rankings)
+
+calRankings(weights)
--- tests/testRanking/graphics.py
+++ tests/testRanking/graphics.py
@@ -12,8 +12,14 @@ with open('rankingResults140k.tsv') as f:
     reader = csv.DictReader(f) # read rows into a dictionary format
     for row in reader: # read a row as {column1: value1, column2: value2,...}
         for (k,v) in row.items(): # go over each column name and value
-            columns[k].append(v) # append the value into the appropriate list
+            columns[k].append(v)# append the value into the appropriate list
                                  # based on column name k
+header = []
+with open('rankingResults140k.tsv') as f:
+    reader = csv.DictReader(f)
+    header = f.readline().split(',')
+header[(len(header)-1)] = header[(len(header)-1)][:-1] #remove the breakline
+
 
 CUANTITIESRANGES ={'green': 0, 'yellow' : 0, 'red':0, 'black':0 }
 
@@ -37,14 +43,17 @@ for key, val in columns.items():
     results[key] = getCuentitiesRanges(val,0.95,0.8)
 
 
-print(results)
+
 N = 5
-Green = (results['Original']['green'], results['w1(t:0.65 a:0.2 y:0.15)']['green'],
-         results['w2(t:0.1 a:0.9 y:0.0)']['green'], results['w3(t:0.1 a:0.0 y:0.9)']['green'], results['w4(t:0.9 a:0.05 y:0.05)']['green'])
-Yellow = (results['Original']['yellow'], results['w1(t:0.65 a:0.2 y:0.15)']['yellow'],
-          results['w2(t:0.1 a:0.9 y:0.0)']['yellow'],results['w3(t:0.1 a:0.0 y:0.9)']['yellow'], results['w4(t:0.9 a:0.05 y:0.05)']['yellow'])
-Red = (results['Original']['red'], results['w1(t:0.65 a:0.2 y:0.15)']['red'],
-       results['w2(t:0.1 a:0.9 y:0.0)']['red'], results['w3(t:0.1 a:0.0 y:0.9)']['red'], results['w4(t:0.9 a:0.05 y:0.05)']['red'])
+
+Green = []
+Yellow = []
+Red = []
+
+for head in header:
+    Green.append(results[head]['green'])
+    Yellow.append(results[head]['yellow'])
+    Red.append(results[head]['red'])
 
 ind = np.arange(N)  # the x locations for the groups
 width = 0.25       # the width of the bars
@@ -60,7 +69,7 @@ rects3 = ax.bar(ind + 2*width, Red, width, color='r')
 ax.set_ylabel('Cantidad')
 ax.set_title('Cantidad de referencias según distintos rankings, muestra = 140k')
 ax.set_xticks(ind + 1.5*width)
-ax.set_xticklabels(('Original', 't:0.65 a:0.2 y:0.15', 't:0.1 a:0.9 y:0.0', 't:0.1 a:0.0 y:0.9', 't:0.9 a:0.05 y:0.05'))
+ax.set_xticklabels(header)
 
 ax.legend((rects1[0], rects2[0],rects3[0]), ('Mayor o igual a 0.95', 'Entre 0.95 y 0.8','Menor a 0.8'))
 
