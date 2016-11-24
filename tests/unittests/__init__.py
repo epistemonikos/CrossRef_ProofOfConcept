544d6166b8c7bc85cfbe3614b00683f4ff8ff8bb
Felipe Rubilar <felipe.rubilar@ug.uchile.cl>
2016-11-24 10:00:47 -0300

    Rename ScienceDirectParserTest + missing tests added to init as comments

--- tests/unittests/__init__.py
+++ tests/unittests/__init__.py
@@ -9,6 +9,8 @@ from tests.unittests.parsertest import BaseParserTest
 from tests.unittests.bmc_test import BMCParserTest
 from tests.unittests.springer_test import SpringerParserTest
 from tests.unittests.wiley_test import WileyParserTest
+# from tests.unittests.plos_test import PlosParserTest
+# from tests.unittests.sciencedirect_test import ScienceDirectParserTest
 
 if __name__ == '__main__':
     unittest.main()
