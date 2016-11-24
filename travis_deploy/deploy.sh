7880ba98129b0573e2a27c01abb8ca5fdfc57c74
Ricardo Matías Peralta Muñoz <mperalta92@gmail.com>
2016-11-24 09:39:08 -0300

    comentarios sobre los script de deploy

--- travis_deploy/deploy.sh
+++ travis_deploy/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#Script for deploy services with travis, using the remote_commands_deploy.sh file
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     echo "Starting deploy."
     echo "Packing distribution package..."
