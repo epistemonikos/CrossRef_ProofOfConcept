9a6d68df87a8f131352834b63d3127dac1dd6342
Ricardo Peralta <mperalta92@gmail.com>
2016-11-29 15:10:11 -0300

    FIX: swat algorithm works well

--- reflookup/utils/swat/swat.py
+++ reflookup/utils/swat/swat.py
@@ -4,18 +4,42 @@ from reflookup.resources.lookup_functions.citation_extract import getRefID, getR
 import json
 #Paso 1: leer tsv
 dump = open('./src/dump_pdq_chechis.tsv')
-
+output= open('./result/input_grafo.txt', 'w')
 line = dump.readline()
 n = 0
+
+
+def get_param(param, res_parser, res_scopus, res_pubmed):
+    res = None
+    if param == "references":
+        try:
+            res = res_parser[param] + res_scopus.get(param, []) + res_pubmed
+        except:
+            res = None
+    elif isinstance(param, dict):
+        try:
+            res_parser["publication_info"][param["publication_info"]] + res_scopus.get("publication_info")[param["publication_info"]]
+        except:
+            res = None
+
+    else:
+        try:
+            res = res_parser[param] or res_scopus.get(param)
+        except:
+            res = None
+    return res
+
+
 while line:
+
     # por cada linea, resolver referencias con los servicios de la API
     [episteID, doi, pmid] = line.split('\t') # episteID, doi, pmid
-    res_parser = []
+    res_parser = {}
     try:
         res_parser = parser.parse(doi)
     except:
         pass
-    res_scopus = []
+    res_scopus = {}
     try:
         res_scopus = get_scopus_ref(doi)
     except:
@@ -25,36 +49,48 @@ while line:
         res_pubmed = getReferenceInfo(getRefID(pmid))
     except:
         pass
-    if n == 2:
-        print(str(res_parser) + "\n")
-        print(str(res_scopus) + "\n")
-        print(str(res_pubmed) + "\n")
-    #imprimir a archivo nuevo llamado input_grafo.txt
+    #imprimir a archivo nuevo llamado
+    #  input_grafo.txt
+    if n < 5:
+        import json
+        print(json.dumps(res_parser))
+        print(json.dumps(res_pubmed))
+        print(json.dumps(res_scopus))
+        print(episteID)
+        print(doi)
+        print(pmid)
     j = {
-        "title": res_parser["title"] or res_scopus.get("title"),
-        "authors": res_parser["authors"] or res_scopus.get("authors"),
-        "abstract": res_parser["abstract"] or res_scopus.get("abstract"),
+        "title": get_param("title",res_parser ,res_scopus, res_pubmed),
+        "authors": get_param("authors", res_parser, res_scopus, res_pubmed),
+        "abstract": get_param("abstract", res_parser, res_scopus, res_pubmed),
         "ids": {
             "doi": doi,
             "pmid": pmid,
             "episteId": episteID
         },
-        "keywords": res_parser["keywords"] or res_scopus.get("keywords"),
+        "keywords": get_param("keywords", res_parser, res_scopus, res_pubmed),
         "publication_info": {
-            "issue": res_parser["publication_info"]["issue"] or res_scopus.get("publication_info")["issue"],
-            "journal": res_parser["publication_info"]["journal"] or res_scopus.get("publication_info")["journal"],
-            "year": res_parser["publication_info"]["year"] or res_scopus.get("publication_info")["year"],
-            "volume": res_parser["publication_info"]["volume"] or res_scopus.get("publication_info")["volume"],
-            "pages": res_parser["publication_info"]["pages"] or res_scopus.get("publication_info")["pages"]
-        } if res_scopus.get("publication_info") else res_parser["publication_info"],
-        "citation": res_parser["citation"] or res_scopus.get("citation"),
-        "references": res_parser["references"] + res_scopus.get("references", []) + res_pubmed
+            "issue": get_param({"publication_info": "issue"}, res_parser, res_scopus, res_pubmed),
+            "journal": get_param({"publication_info": "journal"}, res_parser, res_scopus, res_pubmed),
+            "year": get_param({"publication_info": "year"}, res_parser, res_scopus, res_pubmed),
+            "volume": get_param({"publication_info": "volume"}, res_parser, res_scopus, res_pubmed),
+            "pages": get_param({"publication_info": "pages"}, res_parser, res_scopus, res_pubmed),
+        },
+        "citation": get_param("citation", res_parser, res_scopus, res_pubmed),
+        "references": get_param("references", res_parser, res_scopus, res_pubmed)
     }
-    line = dump.readline()
     n +=1
-    if n == 3:
-        print(json.dumps(j))
+    if n%10 == 0:
+        print("Cantidad de RS resueltas: "+str(n)+"\n")
+    output.write(json.dumps(j)+'\n')
+    line = dump.readline()
+    if n%5 == 0:
         break
+
+if output:
+    output.close()
+
+
 
 
 
