c5af848375280bd9d42b356aaf6afc643ec5d286
Manuel Olguin <molguin@dcc.uchile.cl>
2016-11-30 11:39:05 -0300

    refactored: extracted functions to separate file for easier reuse

---
+++ reflookup/resources/search_v2/functions.py
@@ -0,0 +1,63 @@
+from threading import Thread
+
+from flask.ext.restful import abort
+
+from reflookup.resources.lookup_functions.citation_search import \
+    cr_citation_lookup, mendeley_lookup
+from reflookup.utils.pubmed_id import getPubMedID
+from reflookup.utils.rating.chooser import Chooser
+from reflookup.utils.standardize_json import StandardDict
+
+
+def single_search(cit, cr_only=False, md_only=False, dont_choose=False):
+    if cr_only and not md_only:
+        return cr_citation_lookup(cit)
+
+    elif md_only and not cr_only:
+        return mendeley_lookup(cit)
+
+    else:
+        results = {
+            'mendeley': None,
+            'crossref': None
+        }
+
+        def cr_thread():
+            results['crossref'] = cr_citation_lookup(cit)
+
+        def md_thread():
+            results['mendeley'] = mendeley_lookup(cit)
+
+        t1 = Thread(target=cr_thread)
+        t2 = Thread(target=md_thread)
+
+        t1.start()
+        t2.start()
+
+        t1.join()
+        t2.join()
+
+        cr = results.get('crossref', StandardDict().getEmpty())
+        md = results.get('mendeley', StandardDict().getEmpty())
+
+        if not cr and not md:
+            abort(404,
+                  message='No results found for query {}'.format(cit))
+        elif not md:
+            return cr
+        elif not cr:
+            return md
+        elif dont_choose:
+            return [cr, md]
+        else:
+            ch = Chooser(cit, [cr, md])
+
+            return ch.select()
+
+
+def deferred_search(citations):
+    results = []
+    for citation in citations:
+        results.append(getPubMedID(single_search(citation)))
+
+    return results
--- reflookup/resources/search_v2/views.py
+++ reflookup/resources/search_v2/views.py
@@ -1,14 +1,7 @@
-from flask_restful import abort
-
-from reflookup.utils.rating.chooser import Chooser
+from reflookup.resources.search_v2.functions import single_search, \
+    deferred_search
 from reflookup.utils.restful.utils import DeferredResource, \
     b64_encode_response, find_pubmedid_wrapper
-from reflookup.resources.lookup_functions.citation_search import \
-    cr_citation_lookup, mendeley_lookup
-from threading import Thread
-from reflookup.utils.pubmed_id import getPubMedID
-
-from reflookup.utils.standardize_json import StandardDict
 
 
 class IntegratedReferenceSearchV2(DeferredResource):
@@ -39,55 +32,3 @@ class IntegratedReferenceSearchV2(DeferredResource):
                 deferred_search, cit)
 
 
-def single_search(cit, cr_only=False, md_only=False, dont_choose=False):
-    if cr_only and not md_only:
-        return cr_citation_lookup(cit)
-
-    elif md_only and not cr_only:
-        return mendeley_lookup(cit)
-
-    else:
-        results = {
-            'mendeley': None,
-            'crossref': None
-        }
-
-        def cr_thread():
-            results['crossref'] = cr_citation_lookup(cit)
-
-        def md_thread():
-            results['mendeley'] = mendeley_lookup(cit)
-
-        t1 = Thread(target=cr_thread)
-        t2 = Thread(target=md_thread)
-
-        t1.start()
-        t2.start()
-
-        t1.join()
-        t2.join()
-
-        cr = results.get('crossref', StandardDict().getEmpty())
-        md = results.get('mendeley', StandardDict().getEmpty())
-
-        if not cr and not md:
-            abort(404,
-                  message='No results found for query {}'.format(cit))
-        elif not md:
-            return cr
-        elif not cr:
-            return md
-        elif dont_choose:
-            return [cr, md]
-        else:
-            ch = Chooser(cit, [cr, md])
-
-            return ch.select()
-
-
-def deferred_search(citations):
-    results = []
-    for citation in citations:
-        results.append(getPubMedID(single_search(citation)))
-
-    return results
