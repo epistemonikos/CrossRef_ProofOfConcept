48f5e05070d2dc031e3d25be1761c3b37a1d1817
Manuel Olguin <molguin@dcc.uchile.cl>
2016-11-30 13:08:40 -0300

    standardized extract references

--- reflookup/resources/extract_v2/functions.py
+++ reflookup/resources/extract_v2/functions.py
@@ -6,32 +6,40 @@ from reflookup.utils.pubmed_id import getPubMedID
 
 
 def extract_refs(dois, urls, pmids):
-    results = []
+    results = {}
 
     def gen_error(id):
         return {'message': 'No references found for {}.'.format(id)}
 
     if dois:
         for doi in dois:
+            results[doi] = []
             try:
                 r = get_scopus_references(doi)
-                results.append(getPubMedID(r))
+                for ref in r.get('references', []):
+                    results[doi].append(getPubMedID(ref))
+
             except HTTPException:
-                results.append(gen_error(doi))
+                results[doi] = gen_error(doi)
     if urls:
         for url in urls:
+            results[url] = []
             try:
                 r = get_scopus_references(url)
-                results.append(getPubMedID(r))
+                for ref in r.get('references', []):
+                    results[url].append(getPubMedID(ref))
+
             except HTTPException:
-                results.append(gen_error(url))
+                results[url] = gen_error(url)
 
     if pmids:
         for pmid in pmids:
+            results[pmid] = []
             try:
                 r = getReferenceInfo(getRefID(pmid))
-                results.append(r)
+                for ref in r:
+                    results[pmid].append(ref)
             except HTTPException:
-                results.append(gen_error(pmid))
+                results[pmid] = gen_error(pmid)
 
     return results
