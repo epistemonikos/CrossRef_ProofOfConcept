b295bfa155b91a98bb16afbc67cc89161f75a275
Manuel Olguin <molguin@dcc.uchile.cl>
2016-12-01 17:22:41 -0300

    fix results -> if list is empty return error message

--- reflookup/resources/extract_v2/functions.py
+++ reflookup/resources/extract_v2/functions.py
@@ -16,8 +16,12 @@ def extract_refs(dois, urls, pmids):
             results[doi] = []
             try:
                 r = get_scopus_references(doi)
-                for ref in r.get('references', []):
-                    results[doi].append(getPubMedID(ref))
+                refs = r.get('references', None)
+                if not refs:
+                    results[doi] = gen_error(doi)
+                else:
+                    for ref in refs:
+                        results[doi].append(getPubMedID(ref))
 
             except HTTPException:
                 results[doi] = gen_error(doi)
@@ -26,8 +30,12 @@ def extract_refs(dois, urls, pmids):
             results[url] = []
             try:
                 r = get_scopus_references(url)
-                for ref in r.get('references', []):
-                    results[url].append(getPubMedID(ref))
+                refs = r.get('references', None)
+                if not refs:
+                    results[url] = gen_error(url)
+                else:
+                    for ref in refs:
+                        results[url].append(getPubMedID(ref))
 
             except HTTPException:
                 results[url] = gen_error(url)
@@ -37,8 +45,13 @@ def extract_refs(dois, urls, pmids):
             results[pmid] = []
             try:
                 r = getReferenceInfo(getRefID(pmid))
-                for ref in r:
-                    results[pmid].append(ref)
+
+                if not r:
+                    results[pmid] = gen_error(pmid)
+                else:
+                    for ref in r:
+                        results[pmid].append(ref)
+
             except HTTPException:
                 results[pmid] = gen_error(pmid)
 
