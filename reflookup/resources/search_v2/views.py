ba566440aad2087decca0d52a75639f18b3e4a9b
Manuel Olguin <molguin@dcc.uchile.cl>
2016-12-07 15:06:33 -0300

    fixed auth_required decorator.

--- reflookup/resources/search_v2/views.py
+++ reflookup/resources/search_v2/views.py
@@ -3,6 +3,8 @@ from reflookup.resources.search_v2.functions import single_search, \
 from reflookup.utils.restful.utils import DeferredResource, \
     b64_encode_response, find_pubmedid_wrapper
 
+from reflookup.auth.models import auth_required
+
 
 class IntegratedReferenceSearchV2(DeferredResource):
     """
@@ -57,10 +59,12 @@ class IntegratedReferenceSearchV2(DeferredResource):
             return b64_encode_response(self.enqueue_job_and_return)(
                 deferred_search, cit)
 
+    @auth_required()
     def get(self):
         args = self.get_parser.parse_args()
         return self.search(args)
 
+    @auth_required()
     def post(self):
         args = self.post_parser.parse_args()
         return self.search(args)
