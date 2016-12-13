ab9393877032de8c67404e37f3197a9cf1da2341
Manuel Olguin <molguin@dcc.uchile.cl>
2016-12-13 16:31:16 -0300

    added parameters to run commands directly on db_manage

--- db_manage.py
+++ db_manage.py
@@ -2,25 +2,35 @@ from reflookup.auth.models import *
 import click
 
 
+@click.group()
+def menu_group():
+    pass
+
+
+@menu_group.command()
 def create_db():
     db.create_all()
 
 
-def delete_db():
+@menu_group.command()
+def drop_db():
     db.drop_all()
 
 
+@menu_group.command()
 def new_user():
     mail = ''
     passwd = ''
 
     while mail == '' and passwd == '':
         mail = click.prompt('Email', type=str, default='', show_default=False)
-        passwd = click.prompt('Password', type=str, default='', show_default=False)
+        passwd = click.prompt('Password', type=str, default='',
+                              show_default=False)
 
     scope = -1
     while scope != 0 and scope != 1:
-        scope = click.prompt('Scope (0 - admin, 1 - client):', type=int, default=1)
+        scope = click.prompt('Scope (0 - admin, 1 - client):', type=int,
+                             default=1)
 
     c = User(mail, passwd, scope)
     db.session.add(c)
@@ -31,26 +41,28 @@ def new_user():
 
 menu_dict = {
     'c': create_db,
-    'd': delete_db,
+    'd': drop_db,
     'a': new_user,
     'e': exit
 }
 
 
-def menu():
+@menu_group.command()
+def interactive():
     click.echo(
         '''
 Choose an option:
 - (C)reate database.
-- (D)elete database.
+- (D)rop database.
 - (A)dd a new client.
 - (E)xit
         '''
     )
 
-    choice = click.prompt('Enter your choice:', type=str, default='E').lower()
+    choice = click.prompt('Enter your choice:', type=str,
+                          default='E').lower()
     menu_dict[choice]()
 
 
 if __name__ == '__main__':
-    menu()
+    menu_group()
