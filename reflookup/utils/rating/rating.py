887138038e921a0f1cc37adbb6c1191e75e6f86b
Fabian Mosso Chavez <fmosso@klabs.cl>
2016-12-13 17:16:38 -0300

    change regular expresion

--- reflookup/utils/rating/rating.py
+++ reflookup/utils/rating/rating.py
@@ -1,8 +1,8 @@
 from reflookup.utils.rating.ratings.authors_rating import AuthorsRating
 from reflookup.utils.rating.ratings.title_rating import TitleRating
-
 from reflookup.utils.rating.ratings.year_rating import YearRating
-
+from reflookup.utils.rating.ratings.journal_rating import JournalRating
+from reflookup.utils.rating.ratings.pagination_rating import PaginationRating
 
 class Rating:
     def __init__(self, cita, json):
@@ -15,16 +15,25 @@ class Rating:
                           json.get('authors', [])]
         year = json.get('publication_type', {}).get('title', '')
         title = json.get('title', '')
+        journal = json.get('publication_type',{}).get('title','')
+        pagination = json.get('publication_type',{}).get('pagination','')
         self.authors_rating = AuthorsRating(cita, authors_given,
                                             authors_family)
         self.title_rating = TitleRating(cita, title)
         self.year_rating = YearRating(cita, year, title)
+        self.journal_rating = JournalRating(cita,journal)
+        self.pagination = PaginationRating(cita,pagination)
 
     def value(self, verbose=False):
         title_rating = self.title_rating.value()
         authors_rating = self.authors_rating.value()
         year_rating = self.year_rating.value()
-        final_rating = title_rating * 0.65 + authors_rating * 0.25 + year_rating * 0.1
+        journal_rating = self.journal_rating.value()
+        pagination_rating = self.pagination_rating.value()
+        if journal_rating:
+            final_rating = title_rating * 0.35 + authors_rating * 0.1 + year_rating * 0.2 + journal_rating *0.2 + pagination_rating*0.15
+        else:
+            final_rating = title_rating * 0.55 + authors_rating * 0.15 + year_rating * 0.3
         if verbose:
             print(self.cita)
             print(self.json)
