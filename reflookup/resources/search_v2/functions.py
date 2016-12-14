3ca10138dbb979298529675cbcc4867a58ff8a27
Felipe Rubilar <felipe.rubilar@ug.uchile.cl>
2016-12-14 15:56:29 -0300

    Requests CrossRef y Mendeley secuenciales

--- reflookup/resources/search_v2/functions.py
+++ reflookup/resources/search_v2/functions.py
@@ -9,7 +9,7 @@ from reflookup.utils.rating.chooser import Chooser
 from reflookup.utils.standardize_json import StandardDict
 
 
-def single_search(cit, cr_only=False, md_only=False):
+def single_search(cit, cr_only=False, md_only=False, threshold=0.5):
     if cr_only and not md_only:
         return cr_citation_lookup(cit)
 
@@ -17,40 +17,56 @@ def single_search(cit, cr_only=False, md_only=False):
         return mendeley_lookup(cit)
 
     else:
-        results = {
-            'mendeley': None,
-            'crossref': None
-        }
+        # results = {
+        #     'mendeley': None,
+        #     'crossref': None
+        # }
+        #
+        # def cr_thread():
+        #     results['crossref'] = cr_citation_lookup(cit)
+        #
+        # def md_thread():
+        #     results['mendeley'] = mendeley_lookup(cit)
+        #
+        # t1 = Thread(target=cr_thread)
+        # t2 = Thread(target=md_thread)
+        #
+        # t1.start()
+        # t2.start()
+        #
+        # t1.join()
+        # t2.join()
+        #
+        # cr = results.get('crossref', StandardDict().getEmpty())
+        # md = results.get('mendeley', StandardDict().getEmpty())
+        #
+        # if not cr and not md:
+        #     abort(404,
+        #           message='No results found for query {}'.format(cit))
+        # elif not md:
+        #     return cr
+        # elif not cr:
+        #     return md
+        # else:
+        #     ch = Chooser(cit, [cr, md])
+        #     return ch.select()
 
-        def cr_thread():
-            results['crossref'] = cr_citation_lookup(cit)
+        cr_result = cr_citation_lookup(cit)
+        not_cr = cr_result is None
+        if not_cr:
+            cr_result = StandardDict().getEmpty()
 
-        def md_thread():
-            results['mendeley'] = mendeley_lookup(cit)
+        if cr_result.get('rating', {}).get('total', 0.0) < threshold:
+            md_result = mendeley_lookup(cit)
+            not_md = md_result is None
+            if not_cr and not_md:
+                abort(404, message='No results found for query {}'.format(cit))
 
-        t1 = Thread(target=cr_thread)
-        t2 = Thread(target=md_thread)
-
-        t1.start()
-        t2.start()
-
-        t1.join()
-        t2.join()
-
-        cr = results.get('crossref', StandardDict().getEmpty())
-        md = results.get('mendeley', StandardDict().getEmpty())
-
-        if not cr and not md:
-            abort(404,
-                  message='No results found for query {}'.format(cit))
-        elif not md:
-            return cr
-        elif not cr:
-            return md
-        else:
-            ch = Chooser(cit, [cr, md])
-
+            if not_md:
+                return cr_result              
+            ch = Chooser(cit, [cr_result, md_result])
             return ch.select()
+        return cr_result
 
 
 def multi_search(citations, cr_only=False, md_only=False):
