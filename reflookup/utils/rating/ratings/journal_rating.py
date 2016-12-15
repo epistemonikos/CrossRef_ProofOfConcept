489cde47225fe4bdaab8ab0bc8cdf5a0080a9966
Fabian Mosso Chavez <fmosso@klabs.cl>
2016-12-15 15:35:44 -0300

    added fuzzy substring match for journal

--- reflookup/utils/rating/ratings/journal_rating.py
+++ reflookup/utils/rating/ratings/journal_rating.py
@@ -1,8 +1,10 @@
+from fuzzywuzzy import fuzz
+
 class JournalRating:
+
     def __init__(self, raw_cita, journal):
         self.cita = raw_cita or ''
-        self.pagination = journal or ''
+        self.journal = journal or ''
 
     def value(self):
-        #TODO: an aproximate string match for journal and the cite
-        return 1
+        return  fuzz.token_set_ratio(self.journal,self.cita)/100
