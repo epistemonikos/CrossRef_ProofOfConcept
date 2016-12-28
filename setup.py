76d73314b8cfae8a28dd73525ef559bb99267a42
Juan Vasquez <ji.vasquez0@gmail.com>
2016-12-28 17:37:09 -0300

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='ReferenceLookupService',
+	version='1.0',
+	description='Refsolver',
+	author='Chechis boys',
+	author_email='',
+	url='',
+	packages=[],
+	)
